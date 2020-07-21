2535c8685fa2a9474b53bd9d756e6adfbabea582
Sandeep <sandyaveas@gmail.com>
2020-07-21 19:26:37 +0530

    Test change

--- FrontEnd/src/app/property/property-card/property-card.component.ts
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  // Property: any = {
-  //   "Id":1,
-  //   "Name" : "Birla House",
-  //   "Type":"House",
-  //   "Price":5000
-  // }
+  Property: any = {
+    "Id":1,
+    "Name" : "Birla House",
+    "Type":"House",
+    "Price":5000
+  }
 
   constructor() { }
 
